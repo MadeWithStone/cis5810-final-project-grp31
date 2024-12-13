5328352c1f570530939dc4cc38f712cac2623e4f
Maxwell Stone <maxwell@kinetixml.com>
2024-12-13 00:10:05 -0500

    Merge branch 'main' of https://github.com/MadeWithStone/home-run-derby

--- bat_estimation.py
+++ bat_estimation.py
@@ -35,39 +35,40 @@ def convert_player_poses_to_meters(player_poses):
 
 
 def get_bat_end_from_player_poses(player_poses):
-    bat_length = 0.4 # m
+    bat_length = 0.7 # m
     wrist1, wrist2 = player_poses[:, 9], player_poses[:, 10]
+    wrists = (wrist1 + wrist2) / 2
     elbow1, elbow2 = player_poses[:, 7], player_poses[:, 8]
-    forearm1 = wrist1 - elbow1
-    forearm2 = wrist2 - elbow2
+    forearm1 = wrists - elbow1
+    forearm2 = wrists - elbow2
     
     bat = []
-    for forearm1_pos, forearm2_pos, wrist_pos in zip(forearm1, forearm2, wrist1):
+    for forearm1_pos, forearm2_pos, wrist_pos in zip(forearm1, forearm2, wrists):
         cross_prod = np.cross(forearm1_pos, forearm2_pos)
         bat.append(cross_prod / np.linalg.norm(cross_prod) * bat_length + wrist_pos)
-    return np.array(wrist2)
+    return np.array(bat)
 
 
-def get_bat_force(bat_poses, fps=240,
-                  m=0.5):
-    ends_z = bat_poses[:, 0]
+def get_bat_force(bat_poses, fps=120,
+                  m=0.9):
+    ends_x = -bat_poses[:, 0]
 
     # find when bat is pointed most at camera (i.e. greatest x depth difference)
     num_poses = len(bat_poses)
     greatest_depth = 0.0
     j = 0
     for i in range(1, num_poses):
-        z = ends_z[i]
-        prev_z = ends_z[i - 1]
-        depth = prev_z - z
+        x = ends_x[i]
+        prev_x = ends_x[i - 1]
+        depth = prev_x - x
         if depth > greatest_depth:
             greatest_depth = depth
             j = i
 
     # using greatest depth index, determine instantaneous acceleration
     v1 = (bat_poses[j] - bat_poses[j - 1]) / (1/fps) # m/s
-    v0 = (bat_poses[j - 1] - bat_poses[j - 2]) / (1/fps) # m/s
-    a = (v1 - v0) / (1/fps)
+    print("Bat Speed at Contact:\t", np.linalg.norm(v1), "m/s")
+    a = v1 / (1/fps)
     F = m * a
     return F, j
 
@@ -80,26 +81,13 @@ def transform_bat_poses(bat_poses):
     for i in range(len(bat_poses)):
         bat_poses[i] = T @ bat_poses[i]
     return bat_poses
-
+        
 raw_player_poses = np.load("player_poses.npy") # wrists: 9 and 10, elbows: 7 and 8, shoulders: 5 and 6
 player_poses = convert_player_poses_to_meters(raw_player_poses)
 bat_poses = get_bat_end_from_player_poses(player_poses)
 bat_poses = transform_bat_poses(bat_poses)
 F, j = get_bat_force(bat_poses)
-print("F =", np.linalg.norm(F), "N")
+print("Force of Bat:\t\t", np.linalg.norm(F), "N")
 
-trajectory.get_trajectory(bat_poses)
-
-# # vis_data_3d(player_poses[j-1])
-# # vis_data_3d(player_poses[j])
-# # vis_data_3d(player_poses[j+1])
-# # plt.show()
-
-# fig = plt.figure()
-# ax = plt.axes(projection='3d')
-# print(len(bat_poses))
-# ax.plot(bat_poses[:, 2], bat_poses[:, 0], bat_poses[:, 1], color="tab:blue")
-# # ax.scatter(bat_poses[j][2], bat_poses[j][0], bat_poses[j][1], color="tab:orange")
-# # ax.scatter(bat_poses[j+1][2], bat_poses[j+1][0], bat_poses[j+1][1], color="tab:red")
-
+trajectory.get_trajectory(F)
 plt.show()
--- trajectory.py
+++ trajectory.py
@@ -1,15 +1,13 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from matplotlib.animation import FuncAnimation
 import mpl_toolkits.mplot3d.axes3d as p3
-import random
 
 def generate_ball_trajectory(initial_pos, # m
                              bat_force, # vector, N
                              baseball_speed=40.0, # m/s
                              m_baseball=0.142, # kg
                              impulse_time=0.7e-3, # ms
-                             step=0.01, # s
+                             step=0.01 # s
 ):
     # assuming y is parallel to ground and x is perpendicular
     v_baseball_0 = np.array((baseball_speed * np.sqrt(2), baseball_speed * np.sqrt(2), 0))
@@ -34,8 +32,7 @@ def generate_ball_trajectory(initial_pos, # m
 
 
 def animate_matplotlib(positions, # m
-                       space_size, # pixels
-                       time_step=0.01 # s
+                       space_size # pixels
                        ):
     # animation funcions
     fig = plt.figure()
@@ -43,21 +40,12 @@ def animate_matplotlib(positions, # m
 
     ln, = ax.plot(positions[:, 0], positions[:, 1], positions[:, 2])
 
-    def init():
-        x, y, z = positions[0]
-        ax.plot(x, y, z)
-        return ln,
-
-    def update(n):
-        x, y, z = positions[int(n)]
-        ax.plot(x, y, z)
-        return ln,
-
     # Set axis properties
     ax.set_xlim3d([0.0, space_size[0]])
     ax.set_xlabel('X')
 
-    ax.set_ylim3d([0.0, space_size[1]])
+    # for plotting, shift the trajectory y position to middle
+    ax.set_ylim3d([-space_size[1] / 2, space_size[1] / 2])
     ax.set_ylabel('Y')
 
     ax.set_zlim3d([0.0, space_size[2]])
@@ -65,50 +53,19 @@ def animate_matplotlib(positions, # m
 
     plt.show()
 
-    # N = len(positions)
-    # frames = np.arange(0.0, N)
-    # ani = FuncAnimation(fig, update, frames=np.arange(0.0, N, 1.0), init_func=init, interval=time_step, blit=True)
-    # plt.show()
-    #plt.close(fig)
-    #HTML(ani.to_jshtml())
-
-def get_trajectory(bat_poses):
-    mag = random.randint(int(1e3), int(6e3))
-    x = random.randint(0, 30) / 100.0
-    y = random.randint(0, 30) / 100.0
-    z = 1.0 - x - y
-    bat_force = mag * np.array((x, y, z))
+def get_trajectory(bat_force):
     traj = generate_ball_trajectory(
         initial_pos=np.array((0.0, 0.0, 1.0)),
         bat_force=bat_force
     )
+
     animate_matplotlib(
-        traj, np.array((130.0, 130.0, 80.0))
+        traj, np.array((130.0, 130.0, 15.0))
     )
 
 # test
 def test_trajectories():
     get_trajectory()
-    # traj = generate_ball_trajectory(
-    #     initial_pos=np.array((0.0, 0.0, 1.0)),
-    #     bat_force=26e3 * np.array((0.4, 0.4, 0.2))
-    # )
-
-    # # test by projecting into xy and yz
-    # X = traj[:, 0]
-    # Y = traj[:, 1]
-    # Z = traj[:, 2]
-
-    # plt.plot(X, Y)
-    # plt.show()
-    # plt.plot(Y, Z)
-    # plt.show()
-
-    # print(traj)
-
-    # animate_matplotlib(
-    #     traj, np.array((300.0, 300.0, 300.0))
-    # )
 
 if __name__ == "__main__":
     test_trajectories()
